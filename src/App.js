999dc982aab5400c509dc4344d69c0c5ea6df5d1
Shaun O'Neill <oshauneill@hotmail.com>
2021-05-19 23:48:33 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Baseball Game',
+      day: 'August 26th 2021 @ 3:15pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Hockey Game',
+      day: 'October 15th 2021 @ 1:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Basketball Game',
+      day: 'December 21st 2021 @ 7:30pm',
+      reminder: false,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
